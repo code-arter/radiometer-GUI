766f21e365f8647317d034a1e5d9216bc27a81d6
unknown <zhangye@ESG.360ES.CN>
2019-07-22 14:10:40 +0800

    去除水云和冰云选择，默认1d

--- GUI_EXE/process_qt_input.py
+++ GUI_EXE/process_qt_input.py
@@ -84,7 +84,25 @@ class QtConfManager(object):
 
         self.process_multi_val(qt_dict, out_dict)
 
+        self.process_wc_file(qt_dict, out_dict)
+
+        self.process_ic_file(qt_dict, out_dict)
+
         return True, self.out_file_path, out_dict
+
+    def process_wc_file(self, qt_dict, out_dict):
+        key = "wc_file"
+        type = qt_dict.get("wc_file_type", '1d')
+        path= qt_dict.get("wc_file_path", '')
+        if type and path:
+            out_dict[key] = ["{0} {1} {2}".format(key, type, path)]
+
+    def process_ic_file(self, qt_dict, out_dict):
+        key = "ic_file"
+        type = qt_dict.get("ic_file_type", '1d')
+        path= qt_dict.get("ic_file_path", '')
+        if type and path:
+            out_dict[key] = ["{0} {1} {2}".format(key, type, path)]
 
     def process_multi_val(self, qt_dict, out_dict):
         multi_setting = ["mixing_ratio", "mol_modify",
@@ -95,7 +113,10 @@ class QtConfManager(object):
                 continue
             new_list = []
             for val in qt_dict[setting].split("; "):
-                new_val = "%s %s" % (setting, val)
+                if setting == "wc_set" or setting == "ic_set":
+                    new_val = "%s 1d %s" % (setting, val)
+                else:
+                    new_val = "%s %s" % (setting, val)
                 new_list.append(new_val)
             out_dict[setting] = new_list
 
